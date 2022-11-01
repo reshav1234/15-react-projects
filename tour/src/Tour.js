80c81d58790ebd0c95a56a5e81231092dcefa8b1
Reshav <thapareshav50@gmail.com>
2022-11-01 18:50:42 +0545

    comit

--- tour/src/Tour.js
+++ tour/src/Tour.js
@@ -2,16 +2,17 @@ import React, {useState} from 'react'
 
 const Tour = ({id, image, info, price, name, removeTours}) => 
 {
-    const [readMore, setReadMore] = useState(false)
+    
+    const [readMore, setReadMore] =useState(true)
 
     return (
         <article>
             <img src = {image} alt = {name} />
             <p>
-                {readMore ? info : `${info.substring(0, 200)}...`}
-                <button onClick={() => setReadMore(!readMore)}>
-                    {readMore ? 'show less' : '  read more'}
-                </button>
+                
+                {readMore ? info: `${info.substring(0, 200)}...` // If readmore is true then display info
+                }
+                <button onClick = {() => setReadMore(!readMore)}>{readMore ? `show less`:'read more'}</button>
             </p>
             <button onClick = {() => removeTours(id)}>Not interested</button>
         </article>
